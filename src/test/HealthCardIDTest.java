ec1b8196019fbed5aa404beb57d17eb6a5e917e0
nietzche <havocf4u@gmail.com>
2019-12-10 16:41:11 +0100

    HealthCardIDTest done, exceptions need it?

---
+++ src/test/HealthCardIDTest.java
@@ -0,0 +1,31 @@
+package test;
+
+
+import data.HealthCardID;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HealthCardIDTest {
+        @Test
+        public void TesttoString(){
+            HealthCardID hc = new HealthCardID("Test_Code");
+            String hc2 = "HealthCardID{personal code='Test_Code'}";
+            assertEquals(hc2,hc.toString());
+        }
+
+        @Test
+        public void TestEquals(){
+            HealthCardID hc = new HealthCardID("Equals");
+            HealthCardID hc2 = new HealthCardID ("Equals");
+            assertEquals(hc, hc2);
+        }
+
+        @Test
+        public void TestHashCode(){
+            HealthCardID hc = new HealthCardID ("HashCode");
+            HealthCardID hc2 = new HealthCardID ("HashCode");
+            assertEquals(hc.hashCode(), hc2.hashCode());
+        }
+
+}
